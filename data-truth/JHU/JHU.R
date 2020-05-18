cc131fab4d29daac3a6673a4b6232a0c2ff18cbe
jbracher <jbracher@outlook.de>
2020-05-18 12:07:15 +0200

    Automating JHU data processing

--- data-truth/JHU/JHU.R
+++ data-truth/JHU/JHU.R
@@ -9,16 +9,23 @@
 
 ##Script to Process Raw Deaths File from Johns Hopkins University
 
-jhu_raw<-read.csv("time_series_covid19_deaths_global.csv",stringsAsFactors = FALSE)
+jhu_raw<-read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",stringsAsFactors = FALSE)
 jhu_germany<-jhu_raw[jhu_raw$Country.Region=="Germany",]
-dates<-seq(as.Date("2020-01-22"),as.Date("2020-05-08"),by="days")
-jhu_truth_germany_cum<-data.frame(date=dates,location=rep("GM",length(dates)),
-                        location_name=rep("Germany",length(dates)),
-                        value=as.numeric(t(jhu_germany[1,-c(1:4)])))
-inc_deaths<-diff(jhu_truth_germany_cum$value,lag=1)
 
-jhu_truth_germany_inc<-jhu_truth_germany_cum
-jhu_truth_germany_inc$value<-c(0,inc_deaths)
+cols_dates <- colnames(jhu_germany)[grepl("X", colnames(jhu_germany))]
+dates <- as.Date(gsub("X", "", cols_dates), format = "%m.%d.%y")
 
-write.csv(jhu_truth_germany_cum,file="truth_JHU-Cumulative Deaths_Germany.csv",row.names=FALSE)
-write.csv(jhu_truth_germany_inc,file="truth_JHU-Incident Deaths_Germany.csv",row.names=FALSE)
+jhu_truth_germany_cum <- data.frame(date = dates,
+                                    location = "GM",
+                                    location_name = "Germany",
+                                    value = as.numeric(jhu_germany[, cols_dates]))
+
+
+jhu_truth_germany_inc <- data.frame(date = dates[-1],
+                                    location = "GM",
+                                    location_name = "Germany",
+                                    value = diff(as.numeric(jhu_germany[, cols_dates])))
+
+
+write.csv(jhu_truth_germany_cum,file="truth_JHU-Cumulative Deaths_Germany.csv", row.names = FALSE)
+write.csv(jhu_truth_germany_inc,file="truth_JHU-Incident Deaths_Germany.csv", row.names = FALSE)
