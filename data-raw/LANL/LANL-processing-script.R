d30e91451de87a34906d7cd1c39c721ac971cb6c
JADDEUSC <ugiut@student.kit.edu>
2020-05-22 11:17:30 +0200

    latest MIT, LANL and YYG data

--- data-raw/LANL/LANL-processing-script.R
+++ data-raw/LANL/LANL-processing-script.R
@@ -11,7 +11,8 @@
 
 library(tidyverse)
 
-source("process_lanl_file.R")
+Sys.setlocale("LC_TIME", "C")
+
 source("process_global_lanl_file.R")
 
 lanl_filenames <- list.files(".", pattern=".csv", full.names=FALSE)
@@ -21,7 +22,7 @@ dates <- unlist(lapply(lanl_filenames, FUN = function(x) substr(basename(x), 0, 
 for(dat in dates){
   cum_filename <- paste0(dat, "_deaths_quantiles_global_website.csv")
   inc_filename <- paste0(dat, "_deaths_incidence_quantiles_global_website.csv")
-
+  print(cum_filename)
 
   ### 
   ger_cum_filename <- paste0(dat, "_deaths_quantiles_global_website.csv")
