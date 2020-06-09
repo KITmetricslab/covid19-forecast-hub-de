ab15b619b22a5a152fa233886d8e982c9833bd59
jbracher <jbracher@outlook.de>
2020-06-09 17:49:40 +0200

    Adding clear comments to app coed.

--- app_forecasts_de/server.R
+++ app_forecasts_de/server.R
@@ -1,15 +1,7 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(RColorBrewer)
 
+# read in plotting functions etc
 source("code/app_functions.R")
 
 # Choose the right option, depending on your system:
@@ -23,51 +15,61 @@ Sys.setlocale(category = "LC_TIME", locale = "en_US.UTF8")
 
 # ----------------------------------------------------------------------------
 
-forecasts_to_plot <- read.csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/app_forecasts_de/data/forecasts_to_plot.csv")
+# read in data set compiled specificaly for Shiny app:
+forecasts_to_plot <- read.csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/app_forecasts_de/data/forecasts_to_plot.csv",
+                              stringsAsFactors = FALSE)
 forecasts_to_plot$forecast_date <- as.Date(forecasts_to_plot$forecast_date)
 forecasts_to_plot$timezero <- as.Date(forecasts_to_plot$timezero)
 forecasts_to_plot$target_end_date <- as.Date(forecasts_to_plot$target_end_date)
 forecasts_to_plot <- subset(forecasts_to_plot, grepl("cum", target))
 
-# get timezeros
+# get timezeros, i.e. Mondays on which forecasts were made:
 timezeros <- as.character(sort(unique(forecasts_to_plot$timezero), decreasing = TRUE))
 
-# get models:
+# get names of models which appear in the data:
 models <- sort(as.character(unique(forecasts_to_plot$model)))
+
+# assign colours to models (currently restricted to eight):
 cols_models <- brewer.pal(n = 8, name = 'Dark2')
 names(cols_models) <- models
 
 # get truth data:
-
 dat_truth <- list()
-# dat_truth$RKI <- read.csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/RKI/truth-Cumulative%20Deaths_Germany.csv", stringsAsFactors = FALSE)
-# dat_truth$RKI$date <- as.Date(dat_truth$RKI$date)
-dat_truth$ECDC <- read.csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/ECDC/truth_ECDC-Cumulative%20Deaths_Germany.csv", stringsAsFactors = FALSE)
+# ECDC:
+dat_truth$ECDC <- read.csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/ECDC/truth_ECDC-Cumulative%20Deaths_Germany.csv",
+                           stringsAsFactors = FALSE)
 dat_truth$ECDC$date <- as.Date(dat_truth$ECDC$date)
-dat_truth$JHU <- read.csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/JHU/truth_JHU-Cumulative%20Deaths_Germany.csv", stringsAsFactors = FALSE)
+# JHU;
+dat_truth$JHU <- read.csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/JHU/truth_JHU-Cumulative%20Deaths_Germany.csv",
+                          stringsAsFactors = FALSE)
 dat_truth$JHU$date <- as.Date(dat_truth$JHU$date)
 
+# define point shapes for different truth data sources:
 truths <- names(dat_truth)
 pch_full <- c(17, 16)
 pch_empty <- c(2, 1)
 names(pch_full) <- names(pch_empty) <- truths
 
-# get which model uses which truth data:
+# get data on which model uses which truth data:
 truth_data_used0 <- read.csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/app_forecasts_de/data/truth_data_use.csv",
                            stringsAsFactors = FALSE)
 truth_data_used <- truth_data_used0$truth_data
 names(truth_data_used) <- truth_data_used0$model
 
-# Define server logic required to draw a histogram
+# Define server logic:
 shinyServer(function(input, output) {
 
-  coords <- reactiveValues(click = NULL)
+  # reactive values to store various mouse coordinates (prefer this to using coordinates
+  # directly as NULL values can be avoided):
+  coords <- reactiveValues()
+  # single click:
   observe({
     input$coord_click
     if(!is.null(input$coord_click)){
       coords$click <- input$coord_click
     }
   })
+  # brush, i.e. drawing rectangle:
   observe({
     if(!is.null(input$coord_brush)){
       coords$brush <- list(xlim = as.Date(c(input$coord_brush$xmin, input$coord_brush$xmax), origin = "1970-01-01"),
@@ -77,6 +79,7 @@ shinyServer(function(input, output) {
       coords$brush <- list(xlim = NULL, ylim = NULL)
     }
   })
+  # hover:
   observe({
     input$coord_hover
     if(!is.null(input$coord_hover)){
@@ -84,12 +87,16 @@ shinyServer(function(input, output) {
     }
   })
 
+  # reactive values to store options selected through mouse coordinates:
   selected <- reactiveValues()
+  # target_end_date selected by hovering, along with associated truth and point forecast values
   observe({
     if(!is.null(coords$hover$x)){
+      # click_date <- as.Date(round(coords$click$x), origin = "1970-01-01")
       hover_date <- as.Date(round(coords$hover$x), origin = "1970-01-01")
       if(weekdays(hover_date) == "Saturday"){
-        # get date
+        # get dates
+        # selected$timezero <- click_date
         selected$target_end_date <- hover_date
         # get point estimates:
         subs <- subset(forecasts_to_plot,
@@ -110,6 +117,7 @@ shinyServer(function(input, output) {
     }
   })
 
+  # input element for selection of models to show in plot:
   output$inp_select_model <- renderUI(
     checkboxGroupInput("select_models", "Select models to display:",
                        choiceNames = models,
@@ -117,10 +125,12 @@ shinyServer(function(input, output) {
                        selected = models, inline = TRUE)
   )
 
+  # input element to select forecast date:
   output$inp_select_date <- renderUI(
     selectInput("select_date", "Select forecast date:", choices = timezeros)
   )
 
+  # plot (all wrapped up in function plot_forecasts)
   output$plot_forecasts <- renderPlot({
     par(las = 1)
     plot_forecasts(forecasts_to_plot = forecasts_to_plot,
@@ -139,8 +149,6 @@ shinyServer(function(input, output) {
                    }else{
                      coords$brush$xlim[2]
                    },
-                   # start = as.Date("2020-03-01"),
-                   # end = Sys.Date() + 28,
                    ylim = if(is.null(coords$brush$ylim)){
                      c(0, 12000)
                    }else{
@@ -153,6 +161,7 @@ shinyServer(function(input, output) {
                    show_pi = input$show_pi,
                    add_model_past = input$show_model_past,
                    highlight_target_end_date = selected$target_end_date)
+    # add legends manually:
     legend("topleft", col = cols_models, legend = paste0(models, ": ", selected$point_pred), lty = 0, bty = "n",
            pch = ifelse(models %in% input$select_models,
                         pch_full[truth_data_used], pch_empty[truth_data_used]),
