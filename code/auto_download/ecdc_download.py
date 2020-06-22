22556423daec78117a2b0d299400530def8afa4d
dwolffram <daniel-wolffram@gmx.de>
2020-06-22 10:16:15 +0200

    temporary use of excel data

--- code/auto_download/ecdc_download.py
+++ code/auto_download/ecdc_download.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('https://opendata.ecdc.europa.eu/covid19/casedistribution/csv')
+# df = pd.read_csv('https://opendata.ecdc.europa.eu/covid19/casedistribution/csv')
+df = pd.read_excel('https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide.xlsx')
 current_date = pd.to_datetime('today').date()
 df.to_csv('../../data-truth/ECDC/raw/' + str(current_date) + '-Deaths-ECDC.csv', index=False)
