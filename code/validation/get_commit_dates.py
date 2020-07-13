2bd7ca9612d5bff2fa1c89e41320cfdc0c58af50
dwolffram <daniel-wolffram@gmx.de>
2020-07-13 17:54:25 +0200

    also extract date of first commit

--- code/validation/get_commit_dates.py
+++ code/validation/get_commit_dates.py
@@ -4,11 +4,13 @@ import pandas as pd
 repo = git.Repo("../../")
 tree = repo.tree()
 
-commit_dates = pd.DataFrame(columns=['filename', 'latest_commit_date'])
+commit_dates = pd.DataFrame(columns=['filename', 'first_commit', 'latest_commit'])
 
 for blob in tree.traverse():
     if (blob.path.startswith('data-processed') & blob.path.endswith('.csv')):
-        commit = next(repo.iter_commits(paths=blob.path))
-        commit_dates.loc[len(commit_dates)] = [blob.path.split("/")[-1], str(pd.to_datetime(commit.committed_date, unit='s').date())]
+        commits = list(repo.iter_commits(paths=blob.path))
+        commit_dates.loc[len(commit_dates)] = [blob.path.split("/")[-1], # filename
+                                               str(pd.to_datetime(commits[-1].committed_date, unit='s').date()), # first commit
+                                               str(pd.to_datetime(commits[0].committed_date, unit='s').date())] # latest commit
         
 commit_dates.to_csv('commit_dates.csv', index=False)
