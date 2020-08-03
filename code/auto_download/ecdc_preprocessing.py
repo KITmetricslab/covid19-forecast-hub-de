00ebdb6e4c1604061187401980192286bf5e0095
dwolffram <daniel-wolffram@gmx.de>
2020-08-03 19:30:18 +0200

    extract case data from ecdc for GM + PL

--- code/auto_download/ecdc_preprocessing.py
+++ code/auto_download/ecdc_preprocessing.py
@@ -34,4 +34,11 @@ def preprocess_by_country(country_names=['Germany'], fips_codes=['GM']):
         # export cumulative deaths
         df.to_csv('../../data-truth/ECDC/truth_ECDC-Cumulative Deaths_{}.csv'.format(country), index=False)
         
+        # cases
+        df = df_all[df_all.countriesAndTerritories == country].copy()
+        df.rename(columns={'countriesAndTerritories' : 'location_name', 'cases' : 'value', }, inplace=True)
+        df['location'] = code
+        df = df[['date', 'location', 'location_name', 'value']].sort_values('date').reset_index(drop=True)
+        df.to_csv('../../data-truth/ECDC/truth_ECDC-Cases_{}.csv'.format(country), index=False)
+        
 preprocess_by_country(['Germany', 'Poland'], ['GM', 'PL'])
