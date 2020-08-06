42132e29466f969d9820f222e835a08004fb1a03
jbracher <jbracher@outlook.de>
2020-08-06 10:30:02 +0200

    Updating app.

--- app_forecasts_de/server.R
+++ app_forecasts_de/server.R
@@ -21,7 +21,6 @@ forecasts_to_plot <- read.csv("https://raw.githubusercontent.com/KITmetricslab/c
 forecasts_to_plot$forecast_date <- as.Date(forecasts_to_plot$forecast_date)
 forecasts_to_plot$timezero <- as.Date(forecasts_to_plot$timezero)
 forecasts_to_plot$target_end_date <- as.Date(forecasts_to_plot$target_end_date)
-forecasts_to_plot <- subset(forecasts_to_plot, grepl("cum", target))
 
 # exclude some models because used data is neither ECDC nor JHU:
 models_to_exclude <- c("LeipzigIMISE-rkiV1", "LeipzigIMISE-ecdcV1", "Imperial-ensemble1")
@@ -51,20 +50,16 @@ names(cols_models) <- models
 
 # get truth data:
 dat_truth <- list()
-# ECDC:
-dat_truth_ECDC_Germany <- read.csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/RKI/truth_RKI-Cumulative%20Deaths_Germany.csv",
-         stringsAsFactors = FALSE)
-dat_truth_ECDC_Poland <- read.csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/ECDC/truth_ECDC-Cumulative%20Deaths_Poland.csv",
-                                  stringsAsFactors = FALSE)
-dat_truth$ECDC <- rbind(dat_truth_ECDC_Germany, dat_truth_ECDC_Poland)
-dat_truth$ECDC$date <- as.Date(dat_truth$ECDC$date)
-# JHU;
-dat_truth_JHU_Germany <- read.csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/JHU/truth_JHU-Cumulative%20Deaths_Germany.csv",
-                                  stringsAsFactors = FALSE)
-dat_truth_JHU_Poland <- read.csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/JHU/truth_JHU-Cumulative%20Deaths_Poland.csv",
-                                  stringsAsFactors = FALSE)
-dat_truth$JHU <- rbind(dat_truth_JHU_Germany, dat_truth_JHU_Poland)
-dat_truth$JHU$date <- as.Date(dat_truth$JHU$date)
+dat_truth$JHU <- get_truths(file_cum_death = "https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/JHU/truth_JHU-Cumulative%20Deaths_Germany.csv",
+                            file_inc_death = "https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/JHU/truth_JHU-Incident%20Deaths_Germany.csv",
+                            file_cum_case = "https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/JHU/truth_JHU-Cumulative%20Cases_Germany.csv",
+                            file_inc_case = "https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/JHU/truth_JHU-Incident%20Cases_Germany.csv")
+
+dat_truth$ECDC <- get_truths(file_cum_death = "https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/RKI/truth_RKI-Cumulative%20Deaths_Germany.csv",
+                             file_inc_death = "https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/RKI/truth_RKI-Incident%20Deaths_Germany.csv",
+                             file_cum_case = "https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/RKI/truth_RKI-Cumulative%20Cases_Germany.csv",
+                             file_inc_case = "https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/RKI/truth_RKI-Incident%20Cases_Germany.csv")
+
 
 # define point shapes for different truth data sources:
 truths <- names(dat_truth)
@@ -118,22 +113,22 @@ shinyServer(function(input, output) {
       hover_date <- as.Date(round(coords$hover$x), origin = "1970-01-01")
       if(weekdays(hover_date) == "Saturday"){
         # get dates
-        # selected$timezero <- click_date
         selected$target_end_date <- hover_date
         # get point estimates:
         subs <- subset(forecasts_to_plot,
                        timezero == as.Date(input$select_date) &
+                         grepl(input$select_target, target) &
                          location == input$select_location &
                          target_end_date == hover_date & type %in% c("point", "observed"))
-
         point_pred <- data.frame(model = models)
         point_pred <- merge(point_pred, subs, by = "model", all.x = TRUE)
         selected$point_pred <- round(point_pred$value)
 
+        # get truths:
         selected$truths <- c(subset(dat_truth$ECDC, date == as.Date(selected$target_end_date) &
-                                      location == input$select_location)$value,
+                                      location == input$select_location)[, input$select_target],
                              subset(dat_truth$JHU, date == as.Date(selected$target_end_date) &
-                                      location == input$select_location)$value)
+                                      location == input$select_location)[, input$select_target])
       }else{
         selected$target_end_date <- NULL
         selected$point_pred <- NULL
@@ -155,7 +150,7 @@ shinyServer(function(input, output) {
     selectInput("select_date", "Select forecast date:", choices = timezeros)
   )
 
-  # input element to select forecast date:
+  # input element to select location:
   output$inp_select_location <- renderUI(
     selectInput("select_location", "Select location:", choices = locations, selected = "GM")
   )
@@ -170,13 +165,15 @@ shinyServer(function(input, output) {
         if(is.null(input$select_location)){
           c(0, 12000)
         }else{
-          c(0, 1.2*max(dat_truth$ECDC$value[dat_truth$ECDC$location == input$select_location]))
+          c(0, 1.2*max(dat_truth$ECDC[dat_truth$ECDC$location == input$select_location, input$select_target]))
         }
       }else{
         coords$brush$ylim
       }
+
     plot_forecasts(forecasts_to_plot = forecasts_to_plot,
                    truth = dat_truth,
+                   target = input$select_target,
                    timezero = if(is.null(input$select_date)){as.Date("2020-06-01")}else{as.Date(input$select_date)},
                    models = input$select_models,
                    location = input$select_location,
@@ -200,6 +197,8 @@ shinyServer(function(input, output) {
                    show_pi = input$show_pi,
                    add_model_past = input$show_model_past,
                    highlight_target_end_date = selected$target_end_date)
+    abline(h = 0)
+
     # add legends manually:
     legend("topleft", col = cols_models, legend = paste0(models, ": ", selected$point_pred), lty = 0, bty = "n",
            pch = ifelse(models %in% input$select_models,
