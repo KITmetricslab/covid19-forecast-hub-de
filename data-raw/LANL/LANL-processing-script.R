af80ca3cbca3248701b59e9a3cbddea07505ba4b
Jannik Deuschel <62434864+JADEUSC@users.noreply.github.com>
2020-11-23 17:53:21 +0100

    Update LANL-processing-script.R

--- data-raw/LANL/LANL-processing-script.R
+++ data-raw/LANL/LANL-processing-script.R
@@ -31,7 +31,13 @@
 
 # Warnings can be ignored
 
-library(tidyverse)
+install.packages("crayon", lib="../../Rdeps")
+install.packages("tidyverse", lib="../../Rdeps")
+install.packages("MMWRweek", lib="../../Rdeps")
+install.packages("lubridate", lib="../../Rdeps")
+
+
+library(tidyverse, lib.loc="../../Rdeps")
 
 Sys.setlocale("LC_TIME", "C")
 
